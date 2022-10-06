d87368eaa7fc2eb686b0826d0a2d5895aee91d00
Prabhat <99549878+Skipper76000@users.noreply.github.com>
2022-10-06 09:45:22 +0530

    #include<stdio.h> void printTheArray(int arr[], int n) { 	for (int i = 0; i < n; i++) { 	    printf("%d",arr[i]); 	} 	printf(" "); }
    void generateAllBinaryStrings(int n, int arr[], int i)
    {
    	if (i == n) {
    		printTheArray(arr, n);
    		return;
    	}
    	arr[i] = 0;
    	generateAllBinaryStrings(n, arr, i + 1);
    	arr[i] = 1;
    	generateAllBinaryStrings(n, arr, i + 1);
    }
    
    int main()
    {
    	int n ;
    	scanf("%d",&n);
    
    	int arr[n];
    
    	generateAllBinaryStrings(n, arr, 0);
    
    	return 0;
    }

---
+++ Binary_Number.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+void printTheArray(int arr[], int n)
+{
+	for (int i = 0; i < n; i++) {
+	    printf("%d",arr[i]);
+	}
+	printf("
+");
+}
+
+void generateAllBinaryStrings(int n, int arr[], int i)
+{
+	if (i == n) {
+		printTheArray(arr, n);
+		return;
+	}
+	arr[i] = 0;
+	generateAllBinaryStrings(n, arr, i + 1);
+	arr[i] = 1;
+	generateAllBinaryStrings(n, arr, i + 1);
+}
+
+int main()
+{
+	int n ;
+	scanf("%d",&n);
+
+	int arr[n];
+
+	generateAllBinaryStrings(n, arr, 0);
+
+	return 0;
+}
